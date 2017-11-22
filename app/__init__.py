ae6572c8761c58cde3afcf3ac21886f8ab2ed159
luyisimiger - PythonAnyWhere <luyisimiger@gmail.com>
2017-11-22 18:09:16 +0000

    flask i18n integration with babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
+babel = Babel(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
-from flask import render_template
+from flask import render_template, request
 
 from app import app
+from app import babel
 from app.models import BibleBook, BibleBookChapter, BibleVerse
+from config import LANGUAGES
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(LANGUAGES.keys())
 
 @app.route('/book/<int:id>')
 def book_view(id):
-    
+
     biblebook = BibleBook.query.get(id)
     books = BibleBook.query.all()
     return render_template('bibleapp/book.html', books = books, book = biblebook)
@@ -13,7 +19,7 @@ def book_view(id):
 
 @app.route('/chapters/<int:id>')
 def chapter_view(id=1):
-    
+
     chapter = BibleBookChapter.query.get(id)
     books = BibleBook.query.all()
     return render_template('bibleapp/chapter.html', books = books, chapter = chapter)
@@ -21,7 +27,7 @@ def chapter_view(id=1):
 
 @app.route('/verses/<int:id>')
 def verse_view(id):
-    
+
     verse = BibleVerse.query.get(id)
     books = BibleBook.query.all()
     return render_template('bibleapp/verse.html', books = books, verse = verse)
